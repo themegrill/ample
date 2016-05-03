bae0c59f2eb805019592e12cb37777d0f46eea05
Bishal Napit <mebishalnapit@gmail.com>
2016-05-03 12:39:23 +0545

    Fixing done for JSHint Linting errors

--- js/theme-custom.js
+++ js/theme-custom.js
@@ -1,12 +1,12 @@
 jQuery(document).ready(function(){
 
    // For Search Icon Toggle effect added at the top
-   jQuery(".search-top").click(function(){
-      jQuery("#masthead .search-form-top").toggle();
+   jQuery('.search-top').click(function(){
+      jQuery('#masthead .search-form-top').toggle();
    });
 
    // For Scroll to top button
-   jQuery("#scroll-up").hide();
+   jQuery('#scroll-up').hide();
    jQuery(function () {
       jQuery(window).scroll(function () {
          if (jQuery(this).scrollTop() > 1000) {
