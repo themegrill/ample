c286d1836c658db2645481a93c9d65cdf232b718
RajeebTheGreat <rajeebthegreat@gmail.com>
2016-09-02 16:30:38 +0545

    Grammar Fixes

--- sidebar.php
+++ sidebar.php
@@ -27,7 +27,7 @@
       the_widget( 'WP_Widget_Text',
          array(
             'title'  => __( 'Example Widget', 'ample' ),
-            'text'   => sprintf( __( 'This is an example widget to show how the %s sidebar looks by default. You can add custom widgets from the %swidgets screen%s in the admin. If custom widgets is added than this will be replaced by those widgets', 'ample' ), $sidebar_display, current_user_can( 'edit_theme_options' ) ? '<a href="' . admin_url( 'widgets.php' ) . '">' : '', current_user_can( 'edit_theme_options' ) ? '</a>' : '' ),
+            'text'   => sprintf( __( 'This is an example widget to show how the Left sidebar looks by default. You can add custom widgets from the %swidgets screen%s in the admin. If custom widgets are added then this will be replaced by those widgets', 'ample' ), current_user_can( 'edit_theme_options' ) ? '<a href="' . admin_url( 'widgets.php' ) . '">' : '', current_user_can( 'edit_theme_options' ) ? '</a>' : '' ),
             'filter' => true,
          ),
          array(
