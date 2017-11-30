0f59ed8c45c83971604c29092a915617cb61340d
Ashish Shrestha <ashishsthanp@gmail.com>
2017-11-30 12:53:56 +0545

    Fixed - hamburger menu not working

--- js/navigation.js
+++ js/navigation.js
@@ -7,16 +7,19 @@
 ( function() {
 	var container, button, menu, links, i, len;
 
+	// Nav
 	container = document.getElementById( 'site-navigation' );
 	if ( ! container ) {
 		return;
 	}
 
+	// Hamburger button
 	button = container.getElementsByClassName( 'menu-toggle' )[0];
 	if ( 'undefined' === typeof button ) {
 		return;
 	}
 
+	// first ul inside nav
 	menu = container.getElementsByTagName( 'ul' )[0];
 
 	// Hide menu toggle button if menu is empty and return early.
@@ -26,13 +29,15 @@
 	}
 
 	menu.setAttribute( 'aria-expanded', 'false' );
-	if ( -1 === menu.className.indexOf( 'nav-menu' ) ) {
-		menu.className += ' nav-menu';
+	if ( -1 === menu.className.indexOf( 'menu-primary-container' ) ) {
+		menu.className += ' menu-primary-container';
 	}
 
+	// Hamburger button event
 	button.onclick = function() {
+		// if nav has main-small-navigation class
 		if ( -1 !== container.className.indexOf( 'main-small-navigation' ) ) {
-			container.className = container.className.replace( 'main-small-navigation', 'menu-navigation' );
+			container.className = container.className.replace( 'main-small-navigation', 'main-navigation' );
 			button.setAttribute( 'aria-expanded', 'false' );
 			menu.setAttribute( 'aria-expanded', 'false' );
 		} else {
@@ -43,7 +48,7 @@
 	};
 
 	// Get all the link elements within the menu.
-	links    = menu.getElementsByTagName( 'a' );
+	links = menu.getElementsByTagName( 'a' );
 
 	// Each time a menu link is focused or blurred, toggle focus.
 	for ( i = 0, len = links.length; i < len; i++ ) {
@@ -57,8 +62,8 @@
 	function toggleFocus() {
 		var self = this;
 
-		// Move up through the ancestors of the current link until we hit .nav-menu.
-		while ( -1 === self.className.indexOf( 'nav-menu' ) ) {
+		// Move up through the ancestors of the current link until we hit .menu-primary-container.
+		while ( -1 === self.className.indexOf( 'menu-primary-container' ) ) {
 
 			// On li elements toggle the class .focus.
 			if ( 'li' === self.tagName.toLowerCase() ) {
@@ -78,6 +83,7 @@
 	 */
 	( function( container ) {
 		var touchStartFn, i,
+			// Link with submenu
 			parentLink = container.querySelectorAll( '.menu-item-has-children > a, .page_item_has_children > a' );
 
 		if ( 'ontouchstart' in window ) {
@@ -96,13 +102,15 @@
 				} else {
 					menuItem.classList.remove( 'focus' );
 				}
-			};
+			}; // touchStartFn = function( e ) {
 
+			// on touch in device
 			for ( i = 0; i < parentLink.length; ++i ) {
 				parentLink[i].addEventListener( 'touchstart', touchStartFn, false );
 			}
-		}
+		} // if ( 'ontouchstart' in window ) {
 	}( container ) );
+
 } )();
 
 jQuery(document).ready( function() {
