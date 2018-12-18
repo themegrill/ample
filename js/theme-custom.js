bc4bd45baf681fa78e9a71ae5bc1303d47873af9
Nitu Shrestha <shresthanitu984@gmail.com>
2018-12-18 15:09:27 +0545

    Fix required JS code removed

--- js/theme-custom.js
+++ js/theme-custom.js
@@ -1,8 +1,43 @@
 jQuery( document ).ready( function () {
 
 	// For Search Icon Toggle effect added at the top
+	var hideSearchForm = function () {
+		jQuery( '#masthead .search-form-top' ).removeClass( 'show' );
+	};
+
+	// For Search Icon Toggle effect added at the top
 	jQuery( '.search-top' ).click( function () {
-		jQuery( '#masthead .search-form-top' ).toggle();
+		jQuery( '#masthead .search-form-top' ).toggleClass( 'show' );
+
+		// focus after some time to fix conflict with toggleClass.
+		setTimeout( function () {
+			jQuery( '#masthead .search-form-top.show input' ).focus();
+		}, 200 );
+
+		// For esc key press.
+		jQuery( document ).on( 'keyup', function ( e ) {
+
+			//on esc key press.
+			if ( 27 === e.keyCode ) {
+				//if search box is opened.
+				if ( jQuery( '.search-form-top' ).hasClass( 'show' ) ) {
+					hideSearchForm();
+				}
+			}
+		} );
+
+		// For click out of search box.
+		jQuery( document ).on( 'click.outEvent', function( e ){
+			if ( e.target.closest('.search-form-top') || e.target.closest('.search-top') ) {
+				return;
+			}
+
+			hideSearchForm();
+
+			// Unbind current click event.
+			jQuery( document ).off( 'click.outEvent' );
+		} );
+
 	} );
 
 	// For Scroll to top button
