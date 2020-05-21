11e12e63e4eaebaadfced4cf7773e0740c0b8f84
ThemeGrill <milanthapa299@gmail.com>
2020-05-21 14:22:02 +0545

    Added notice class and linked to functions.php

--- functions.php
+++ functions.php
@@ -170,6 +170,7 @@ define( 'AMPLE_THEME_VERSION', $ample_theme->get( 'Version' ) );
 if ( is_admin() ) {
 	require get_template_directory() . '/inc/admin/class-ample-admin.php';
 	require get_template_directory() . '/inc/admin/class-ample-tdi-notice.php';
+	require get_template_directory() . '/inc/admin/class-ample-notice.php';
 	require get_template_directory() . '/inc/admin/class-ample-welcome-notice.php';
 	require get_template_directory() . '/inc/admin/class-ample-theme-review-notice.php';
 }
