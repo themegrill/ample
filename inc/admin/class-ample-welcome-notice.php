b18334abb3c375e571b8c9b45a3773177c4c39ad
Premiii <jimeepremi@gmail.com>
2021-05-17 15:26:54 +0545

    Fix - Screenshot extension

--- inc/admin/class-ample-welcome-notice.php
+++ inc/admin/class-ample-welcome-notice.php
@@ -43,7 +43,7 @@ class Ample_Welcome_Notice {
 
 			<div class="ample-message__content">
 				<div class="ample-message__image">
-					<img class="ample-screenshot" src="<?php echo esc_url( get_template_directory_uri() ); ?>/screenshot.png" alt="<?php esc_attr_e( 'Ample', 'ample' ); ?>" /><?php // phpcs:ignore WordPress.XSS.EscapeOutput.OutputNotEscaped, Squiz.PHP.EmbeddedPhp.SpacingBeforeClose ?>
+					<img class="ample-screenshot" src="<?php echo esc_url( get_template_directory_uri() ); ?>/screenshot.jpg" alt="<?php esc_attr_e( 'Ample', 'ample' ); ?>" /><?php // phpcs:ignore WordPress.XSS.EscapeOutput.OutputNotEscaped, Squiz.PHP.EmbeddedPhp.SpacingBeforeClose ?>
 				</div>
 
 				<div class="ample-message__text">
