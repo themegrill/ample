9f0903b23b9474ddfdcad2ee64e9b692dbdd21e8
som shrestha <109511507+som3669@users.noreply.github.com>
2025-08-12 10:48:33 +0545

    fix security capability issue (#55)

--- inc/admin/class-ample-admin.php
+++ inc/admin/class-ample-admin.php
@@ -36,8 +36,13 @@ if ( ! class_exists( 'Ample_Admin' ) ) :
 			$welcome_data = array(
 				'uri'      => esc_url( admin_url( '/themes.php?page=demo-importer&browse=all&ample-hide-notice=welcome' ) ),
 				'btn_text' => esc_html__( 'Processing...', 'ample' ),
-				'nonce'    => wp_create_nonce( 'ample_demo_import_nonce' ),
 			);
+
+			// Only add nonce and ajaxurl if user has appropriate capabilities
+			if ( current_user_can( 'manage_options' ) ) {
+				$welcome_data['nonce']   = wp_create_nonce( 'ample_demo_import_nonce' );
+				$welcome_data['ajaxurl'] = admin_url( 'admin-ajax.php' );
+			}
 
 			wp_localize_script( 'ample-plugin-install-helper', 'ampleRedirectDemoPage', $welcome_data );
 		}
